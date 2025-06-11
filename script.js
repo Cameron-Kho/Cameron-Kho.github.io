03cd36a82125b0087ec1672779035078de723b7e
Cameron-Kho <cameron_kho@s.thevillageschool.com>
2025-06-11 01:32:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function showSection(sectionId) {
 
 // Wait for the DOM to be loaded
 document.addEventListener('DOMContentLoaded', function() {
-  const surpriseBtn = document.getElementById("surprise-btn");
+  const surpriseBtn = document.getElementById("god-messages-btn");
   if (surpriseBtn) {
     surpriseBtn.addEventListener("click", function() {
       const fortuneArea = document.getElementById("fortune-result");
@@ -26,40 +26,26 @@ document.addEventListener('DOMContentLoaded', function() {
         <p>Your fortune is...........</p>
       `;
 
-      // Wheel spin    
       if (randomNumber === 1) {
-        resultHTML += `
-          <div class="confetti">Luck is coming! Celebrate.</div>
-          <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3Q5bDNuanZ1amY5ejNxbzh4bmhibmltbzR5YzFvcmMzMHZiNmZ5cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/U56UKYAO8ToW8ycuDe/giphy.gif"
-          alt="Lucky Spin!" style="width:175px;height:175px">
-        `;
+        resultHTML += `<div class="confetti">Luck is coming! Celebrate.</div>
+        <img src="https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExM3Q5bDNuanZ1amY5ejNxbzh4bmhibmltbzR5YzFvcmMzMHZiNmZ5cCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/U56UKYAO8ToW8ycuDe/giphy.gif"
+        alt="Lucky Spin!" style="width:175px;height:175px">`;
       } else if (randomNumber === 2) {
-        resultHTML += `
-          <div class="confetti">Stay humble, and you will be rewarded</div>
-        `;
+        resultHTML += `<div class="confetti">Stay humble, and you will be rewarded</div>`;
       } else if (randomNumber === 3) {
-        resultHTML += `
-          <div class="confetti">This is not your good year, stay wary</div>
-        `;
+        resultHTML += `<div class="confetti">This is not your good year, stay wary</div>`;
       } else if (randomNumber === 4) {
-        resultHTML += `
-          <div class="confetti">Don't take family for granted, spend more time with loved ones.</div>
-        `;
+        resultHTML += `<div class="confetti">Don't take family for granted, spend more time with loved ones.</div>`;
       } else if (randomNumber === 5) {
-        resultHTML += `
-          <div class="confetti">Work hard this year, and you will be blessed next year</div>
-        `;
+        resultHTML += `<div class="confetti">Work hard this year, and you will be blessed next year</div>`;
       } else if (randomNumber === 6) {
-        resultHTML += `
-          <div class="confetti">An opportunity is coming soon, be on the lookout</div>
-        `;
+        resultHTML += `<div class="confetti">An opportunity is coming soon, be on the lookout</div>`;
       } else {
-        resultHTML += `
-          <div class="confetti">Your fortune is confusing, try again</div>
-        `;
+        resultHTML += `<div class="confetti">Your fortune is confusing, try again</div>`;
       }
 
       fortuneArea.innerHTML = resultHTML;
     });
   }
 });
+
